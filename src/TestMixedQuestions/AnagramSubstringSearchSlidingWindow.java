cbbd73c8602bbfa0a4ddb3b6e859cc7a9dcff2e5
ayushi2.sharma <Ayushi2.Sharma@ril.com>
2021-07-25 13:50:14 +0530

    Updated the classes.

--- src/TestMixedQuestions/AnagramSubstringSearchSlidingWindow.java
+++ src/TestMixedQuestions/AnagramSubstringSearchSlidingWindow.java
@@ -2,7 +2,9 @@ package TestMixedQuestions;
 
 import java.util.ArrayList;
 import java.util.List;
-
+//Anagram Substring Search (Or Search for all permutations)
+//Given a text txt[0..n-1] and a pattern pat[0..m-1], write a function search(char pat[], char txt[]) that prints all occurrences of pat[]
+//and its permutations (or anagrams) in txt[]. You may assume that n > m.
 public class AnagramSubstringSearchSlidingWindow {
 
     private static List<Integer> findIndices(String text, String pattern) {
