69e8f3a820bad8007629b9366ae882e631f0b60a
ayushi2.sharma <Ayushi2.Sharma@ril.com>
2021-09-06 23:02:30 +0530

    Slowest Key | Leetcode Sept challenge | Day6

---
+++ src/StackAndQueue/StackImplUsingQueueMethod1.java
@@ -0,0 +1,11 @@
+package StackAndQueue;
+
+//Implement Stack using Queues
+//We are given a Queue data structure that supports standard operations like enqueue() and dequeue().
+//We need to implement a Stack data structure using only instances of Queue and queue operations allowed on the instances.
+//A stack can be implemented using two queues.
+
+
+//Method 1 (By making push operation costly)
+public class StackImplUsingQueueMethod1 {
+}
