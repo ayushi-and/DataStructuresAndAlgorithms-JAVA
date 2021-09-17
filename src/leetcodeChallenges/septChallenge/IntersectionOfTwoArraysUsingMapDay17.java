bc6d8e1fd81b9425a7d1dc6b25c6e45c61477d1a
ayushi2.sharma <Ayushi2.Sharma@ril.com>
2021-09-17 20:00:35 +0530

    Intersection of Two Arrays II | Leetcode Sept challenge | Day17

--- src/leetcodeChallenges/septChallenge/IntersectionOfTwoArraysUsingMapDay17.java
+++ src/leetcodeChallenges/septChallenge/IntersectionOfTwoArraysUsingMapDay17.java
@@ -34,7 +34,7 @@ public class IntersectionOfTwoArraysUsingMapDay17 {
         Map<Integer, Integer> map = new HashMap<>();
 
         for(int i = 0; i < nums1.length; i++) {
-            if(map.containsKey(nums1[1])) {
+            if(map.containsKey(nums1[i])) {
                 map.put(nums1[i], map.get(nums1[i]) + 1);
             }
             else {
