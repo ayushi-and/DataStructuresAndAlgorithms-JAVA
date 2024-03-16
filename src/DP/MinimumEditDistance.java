4febe2d7810ecd30c821ab68e3e308b6fc2ad4d3
ayushi-and <sharma.ayushi9828@gmail.com>
2024-03-16 10:02:05 +0530

    minimum edit distance

--- src/DP/MinimumEditDistance.java
+++ src/DP/MinimumEditDistance.java
@@ -20,4 +20,44 @@ rorse -> rose (remove 'r')
 rose -> ros (remove 'e')
  */
 public class MinimumEditDistance {
+
+    public static void main(String[] args) {
+        String word1 = "horse";
+        String word2 = "ros";
+        System.out.println(minOperations(word1, word2));
+    }
+
+    public static int minOperations(String word1, String word2) {
+
+        int n1 = word1.length();
+        int n2 = word2.length();
+
+        int[][] dp = new int[n1+1][n2+1];
+
+        for(int i = 0; i < n1; i++) {
+            dp[i][0] = i;
+        }
+
+        for(int j = 0; j < n2; j++) {
+            dp[0][j] = j;
+        }
+
+        return minOperation(word1, word2, n1, n2, dp);
+
+    }
+
+    static int minOperation(String word1, String word2, int n1, int n2, int[][] dp) {
+
+        for(int i = 1; i <= n1; i++) {
+            for(int j = 1; j <= n2; j++) {
+                if(word1.charAt(i-1) == word2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else {
+                    dp[i][j] = 1 + Math.min(dp[i-1][j-1], Math.min(dp[i][j-1], dp[i-1][j]));
+                }
+            }
+        }
+        return dp[n1][n2];
+    }
 }
